d4050ab30e0cc5ac0e9b96a0b20980d61b8d8063
ThibaultTostain <thibault.tostain.01@gmail.com>
2021-12-28 14:51:25 +0100

    Ajout ecriture fichier sauvegarde

--- src/main/java/com/example/application/bdd/Ecriture.java
+++ src/main/java/com/example/application/bdd/Ecriture.java
@@ -1,40 +1,31 @@
 package com.example.application.bdd;
 
-import java.sql.Connection;
+import java.io.BufferedWriter;
+import java.io.FileWriter;
+import java.io.IOException;
+import java.sql.*;
 
 public class Ecriture {
 
-    /*public static void ecrireFichier (Connection con) {
+    public static void ecrireFichier (Connection con, int idSemestre) throws SQLException {
+        //
         try {
-            BufferedWriter sauv = new BufferedWriter(new FileWriter(nomDocument+".txt",false));
-            List<Noeud> noeudList = treilli.getNoeudList();
-            List<Barre> barreList = treilli.getBarreList();
-            List<TypeBarre> typeList = treilli.getTypeList();
-            List<TriangleTerrain> sol = terrain.getSol();
-            sauv.write("Terrain;"+terrain.getAbsMin()+";"+terrain.getAbsMax()+";"+terrain.getOrdMin()+";"+terrain.getOrdMax());
-            sauv.newLine();
-            for(int i =0 ; i < sol.size() ; i++) {
-                sauv.write("Triangle;"+sol.get(i).getId()+";"+sol.get(i).getStart()+";"+sol.get(i).getMiddle()+";"+sol.get(i).getEnd()+";"+sol.get(i).getCounter());
-                sauv.newLine();
-                System.out.println(sol.get(i).getId());
+            BufferedWriter sauv = new BufferedWriter(new FileWriter("Choix_Voeux.txt",false));
+            //Ecriture de NG et NC
+            final String requete ="SELECT ng,nc from Semestres WHERE id = 'idSemestre'";
+            try ( Statement st = con.createStatement()) {
+                try ( ResultSet res = st.executeQuery(requete)) {
+                    sauv.write(res.getString(1));
+                    sauv.newLine();
+                    sauv.write(res.getString(1));
+                    sauv.newLine();
+                }
             }
-            sauv.write("Treilli;"+treilli.getId()+";"+treilli.getCount());
-            sauv.newLine();
-            for(int i = 0; i < typeList.size() ; i++) {
-                sauv.write("TypeBarre;"+typeList.get(i).getLongMin()+";"+typeList.get(i).getLongMax()+";"+typeList.get(i).getResTension()+";"+typeList.get(i).getResCompression()+";"+typeList.get(i).getCout()+";"+typeList.get(i).getCount()+";"+typeList.get(i).getId());
-                sauv.newLine();
+            catch (IOException e) {
+                System.out.println("ERROR : ecricreFichier : Ecriture de NG et NC : ecriture impossible : "+e);
             }
-            for(int i = 0 ; i < noeudList.size(); i++) {
-                sauv.write(""+noeudList.get(i));
-                sauv.newLine();
-            }
-            for(int i = 0 ; i < barreList.size(); i++) {
-                sauv.write(""+barreList.get(i));
-                sauv.newLine();
-            }
-            sauv.close(); // permet de fermer la mémoire tampon BufferedWriter
         }
-        catch (IOException err) {System.out.println("Main : ecricreFichier : impossible de créer le fichier");}
-    }*/
+        catch (IOException err) {System.out.println("ERROR : ecricreFichier : impossible de créer le fichier d'écriture");}
+    }
 
 }
