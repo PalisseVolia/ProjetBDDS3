81eb980c875690a98ad91ca22217e710bcd2fbbd
ThibaultTostain <thibault.tostain.01@gmail.com>
2021-12-28 15:13:23 +0100

    Ajout ecriture description modules

--- src/main/java/com/example/application/bdd/Ecriture.java
+++ src/main/java/com/example/application/bdd/Ecriture.java
@@ -10,11 +10,19 @@ public class Ecriture {
     public static void ecrireFichier (Connection con, int idSemestre) throws SQLException {
         //
         try {
-            BufferedWriter sauv = new BufferedWriter(new FileWriter("Choix_Voeux.txt",false));
-            //Ecriture de NG et NC
-            final String requete ="SELECT ng,nc from Semestres WHERE id = 'idSemestre'";
+            BufferedWriter sauv = new BufferedWriter(new FileWriter("Choix_Voeux_idSemestre_"+idSemestre+".txt",false));
+
+
+
+            /*Ecriture de NG et NC
+            *
+            * NG
+            * NC
+            *
+            * */
+            final String requeteA ="SELECT ng,nc from Semestres WHERE id = 'idSemestre'";
             try ( Statement st = con.createStatement()) {
-                try ( ResultSet res = st.executeQuery(requete)) {
+                try ( ResultSet res = st.executeQuery(requeteA)) {
                     sauv.write(res.getString(1));
                     sauv.newLine();
                     sauv.write(res.getString(1));
@@ -24,6 +32,44 @@ public class Ecriture {
             catch (IOException e) {
                 System.out.println("ERROR : ecricreFichier : Ecriture de NG et NC : ecriture impossible : "+e);
             }
+
+
+
+            /*Description des Modules :
+            *
+            * MODULES
+            * Modules.id ; GrpModule.id
+            * Modules.id ; GrpModule.id
+            * Modules.id ; GrpModule.id
+            * FINMODULES
+            *
+            * */
+            sauv.write("MODULES");
+            sauv.newLine();
+            final String requeteB ="SELECT DISTINCT Modules.id,GrpModule.id from Semestres Join GrpModule ON "+idSemestre+" = GrpModule.id Join Modules ON GrpModule.id = Modules.id";
+            try ( Statement st = con.createStatement()) {
+                try ( ResultSet res = st.executeQuery(requeteB)) {
+                    while(res.next()) {
+                        sauv.write(res.getString(1) + ";"+ res.getString(2) );
+                        sauv.newLine();
+                    }
+                }
+            }
+            catch (IOException e) {
+                System.out.println("ERROR : ecricreFichier : Ecriture de NG et NC : ecriture impossible : "+e);
+            }
+            sauv.write("FINMODULES");
+            sauv.newLine();
+
+
+
+            /* Description des choix
+            *
+            * CHOIX
+            * idEtudiant;choixA{idModule},choixB;{Pour chaque étudiants}
+            * FINCHOIX
+            *
+            * */
         }
         catch (IOException err) {System.out.println("ERROR : ecricreFichier : impossible de créer le fichier d'écriture");}
     }
