af40f580cfe30a7144b82e262e500ebb48a64b0d
Volia Palisse <81376099+PalisseVolia@users.noreply.github.com>
2021-12-30 14:45:01 +0100

    changement de page

--- src/main/java/project/VuePrincipale.java
+++ src/main/java/project/VuePrincipale.java
@@ -33,7 +33,7 @@ public class VuePrincipale extends VerticalLayout{
 
         //remplacement par les layouts de la page de Login
         this.setEntete(new LoginPageEntete());
-        this.setMainContent(new LoginPageContent());
+        this.setMainContent(new LoginPageContent(this));
         //style de la page de login
         setHeight("75%");
         setAlignItems(Alignment.CENTER);
@@ -48,6 +48,22 @@ public class VuePrincipale extends VerticalLayout{
     public void setMainContent(Component c) {
         this.mainContent.removeAll();
         this.mainContent.add(c);
+    }
+    public void setAlignment(int value) {
+        switch (value) {
+            case 0:
+            setAlignItems(Alignment.START);
+                break;
+            case 1:
+            setAlignItems(Alignment.CENTER);
+                break;
+            case 2:
+            setAlignItems(Alignment.END);
+                break;
+        
+            default:
+                break;
+        }
     }
         
 }
