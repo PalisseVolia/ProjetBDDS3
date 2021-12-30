b662131e59ff347803ecd62d3ec541d8240b53d2
Volia Palisse <81376099+PalisseVolia@users.noreply.github.com>
2021-12-30 15:03:51 +0100

    Revert "vaadin w/ page de login"
    This reverts commit 1c8069277bf4cf63daeed0cd0e708906db30330d.

--- src/main/java/com/example/application/Application.java
+++ src/main/java/com/example/application/Application.java
@@ -1,7 +1,6 @@
-package project;
+package com.example.application;
 
 import com.vaadin.flow.component.dependency.NpmPackage;
-
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
