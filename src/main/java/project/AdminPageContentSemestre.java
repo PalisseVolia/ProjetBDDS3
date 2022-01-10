dbd6bf335c4373dd429d8ea842137a7c5733854b
watson67 <thibaut.waechter@gmail.com>
2022-01-10 16:07:04 +0100

    jj

--- src/main/java/project/AdminPageContentSemestre.java
+++ src/main/java/project/AdminPageContentSemestre.java
@@ -8,6 +8,8 @@ import com.vaadin.flow.component.button.ButtonVariant;
 import com.vaadin.flow.component.checkbox.CheckboxGroup;
 import com.vaadin.flow.component.orderedlayout.VerticalLayout;
 import com.vaadin.flow.component.textfield.TextArea;
+import com.vaadin.flow.component.notification.Notification;
+import com.vaadin.flow.component.notification.Notification.Position;
 
 import bdd.Commandes;
 
@@ -63,6 +65,8 @@ public class AdminPageContentSemestre extends VerticalLayout{
                     }
                 //TODO: groupe 3 non copié
                 Commandes.NouvSemestre(con, grp1, grp2, grp3);
+                Notification notif = Notification.show("Nouveau semestre créé");
+                notif.setPosition(Position.BOTTOM_CENTER);
             } catch (Exception e) {
                 System.out.println("erreur durant la connexion à la bdd : ajout de semestre IG");
             }
